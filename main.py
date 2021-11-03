d92eed27b0b278c92a48d86f8ed4e0ed7b8c5979
gray5246 <gchatham.f21@warren-wilson.edu>
2021-11-03 15:48:04 -0400

    n

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
     import folium
-    map = folium.Map(location=(35.61, -82.44))
-    map.save("data/map1.html")
+    import csv
+    volcs = open("data/volcanoes.tsv")
+    dialect = csv.Sniffer().sniff(volcs)
