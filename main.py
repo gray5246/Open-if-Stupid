a9ce7c3e0f0986d812e2d84145c71c41d3faa71c
gray5246 <gchatham.f21@warren-wilson.edu>
2021-11-05 15:25:52 -0400

    n

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ if __name__ == '__main__':
     import folium
     import csv
     volcs = open("data/volcanoes.tsv")
-    dialect = csv.Sniffer().sniff(volcs)
+    dialect = csv.Sniffer().sniff(volcs.read(1000))
+    volcs.seek(0)
+    reader = csv.DictReader(volcs)
