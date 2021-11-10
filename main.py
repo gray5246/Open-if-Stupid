ef8b6c6b3b84250af455d1e5f2be2d7f23de9399
gray5246 <gchatham.f21@warren-wilson.edu>
2021-11-10 15:46:30 -0500

    n

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ if __name__ == '__main__':
     volcs = open("data/volcanoes.tsv")
     dialect = csv.Sniffer().sniff(volcs.read(1000))
     volcs.seek(0)
-    reader = csv.DictReader(volcs)
-    rows = []
-    for row in reader:
-        rows.append(row)
-    print(rows)
+    reader = csv.DictReader(volcs, dialect=dialect)
+    rows = list(reader)
+    acm = 0
+    for row in rows:
+        if row['Country'] != "Indonesia":
+            rows.remove(row)
+        acm += 1
+    for row in rows:
+        location = [row['Latitude'], row['Longitude']]
+        mark = folium.Marker(location, icon=folium.Icon(color='red'))
+    map = folium.Map(location=(-8.108, 112.922), zoom_start=5, tiles='Stamen Terrain')
+    map.save('data/map1.html')
+    for row in rows:
+        location = [row['Latitude'], row['Longitude']]
+        mark = folium.Marker(location, icon=folium.Icon(color='red'))
+
